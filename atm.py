f94ba3105c43338452ee59c75c25c27e6b4eb67c
jeremiahlay <jeremiahlay5@gmail.com>
2024-05-01 11:36:43 -0500

    intial commit

---
+++ atm.py
@@ -0,0 +1,25 @@
+# Write a program that mimics an ATM
+
+# set an initial balance
+# then deduct from the account with a withdrawal
+# Note that if you withdrawal more than the balance, your program should notifiy the user of insufficient funds
+
+balance = 2000
+
+amt_due = int(input('Enter the amount to withdrawal: '))
+
+balance = balance - amt_due
+
+print(balance)
+
+if amt_due > balance:
+    print('insufficient funds')
+   
+else:
+    print('please take your cash')
+    print('Avalaible Balance:',(balance))
+
+
+
+
+
